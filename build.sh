85a611f81c5f0105b814c855d7d92324f88450cd
lz-coder <lzcoder@proton.me>
2024-12-26 23:07:45 -0300

    fix: use -y in dnf commands

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 # Turtle for nautilus
-dnf install python3-pygit2 python3-secretstorage nautilus-python meld
+dnf install -y python3-pygit2 python3-secretstorage nautilus-python meld
 git clone https://gitlab.gnome.org/philippun1/turtle.git /tmp/turtle
 python /tmp/turtle/install.py install
 
-dnf install gnome-themes-extra sysprof gitg gstreamer1-plugin-openh264 openh264
-dnf install podman-compose podman-docker podman-tui helix zsh tmux epiphany
-dnf remove noopenh264 firefox firefox-langpacks gnome-software-rpm-ostree rpm-ostree
+dnf install -y gnome-themes-extra sysprof gitg gstreamer1-plugin-openh264 openh264
+dnf install -y podman-compose podman-docker podman-tui helix zsh tmux epiphany
+dnf remove -y noopenh264 firefox firefox-langpacks gnome-software-rpm-ostree rpm-ostree
 
 sed -i '/^PRETTY_NAME/s/Silverblue/Lazyblue/' /usr/lib/os-release
 
