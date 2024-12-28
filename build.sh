178bdbde99ef4d4b6e27258fbb2a16fc7f9ea844
lz-coder <lzcoder@proton.me>
2024-12-28 17:12:57 -0300

    refactor: don't install podman-tui and sysprof pkgs

--- build.sh
+++ build.sh
@@ -9,8 +9,7 @@ dnf install -y python3-pygit2 python3-secretstorage nautilus-python meld
 git clone https://gitlab.gnome.org/philippun1/turtle.git /tmp/turtle
 python /tmp/turtle/install.py install
 
-dnf install -y gnome-themes-extra sysprof gitg gnome-tweaks
-dnf install -y podman-compose podman-docker podman-tui helix tmux epiphany distrobox
+dnf install -y gnome-themes-extra gitg gnome-tweaks podman-compose podman-docker helix tmux epiphany distrobox
 dnf remove -y firefox firefox-langpacks gnome-software-rpm-ostree rpm-ostree
 
 sed -i '/^PRETTY_NAME/s/Silverblue/Lazyblue/' /usr/lib/os-release
