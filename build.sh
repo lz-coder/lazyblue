26df25723be6157fbcff596cd1b575c6bf0da0aa
lz-coder <lzcoder@proton.me>
2024-12-31 19:01:58 -0300

    feat: add fastfetch pkg to base system

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ dnf install -y python3-pygit2 python3-secretstorage nautilus-python meld
 git clone https://gitlab.gnome.org/philippun1/turtle.git /tmp/turtle
 python /tmp/turtle/install.py install
 
-dnf install -y gnome-themes-extra gitg gnome-tweaks podman-compose podman-docker helix tmux epiphany distrobox
+dnf install -y fastfetch gnome-themes-extra gitg gnome-tweaks podman-compose podman-docker helix tmux epiphany distrobox
 dnf remove -y firefox firefox-langpacks gnome-software-rpm-ostree rpm-ostree
 
 sed -i '/^PRETTY_NAME/s/Silverblue/Lazyblue/' /usr/lib/os-release
